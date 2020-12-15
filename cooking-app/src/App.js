844fd98c54e528f1543696cf978bcd9efb4813be
Ajeet Singh <imajeet5@gmail.com>
2020-12-15 16:28:05 +0530

    improved CSS and add and delete recipe functionality

--- cooking-app/src/App.js
+++ cooking-app/src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import RecipeList from './components/RecipeList';
+
+import uuidv4 from 'uuid/dist/v4';
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions:
+      '1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 Pounds',
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: '1. Put paprika on pork\n2. Put pork in oven\n3. Eat pork',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 Pounds',
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs',
+      },
+    ],
+  },
+];
 
 function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [{ id: uuidv4(), name: 'Name', amount: '1 Tbs' }],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecipeList
+      recipes={recipes}
+      handleRecipeAdd={handleRecipeAdd}
+      handleRecipeDelete={handleRecipeDelete}
+    />
   );
 }
 
---
+++ cooking-app/src/components/Ingredient.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Ingredient({ name, amount }) {
+  return (
+    <React.Fragment>
+      <span>{name}</span> <span>{amount}</span>
+    </React.Fragment>
+  );
+}
