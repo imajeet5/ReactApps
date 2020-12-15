b8a19f6875de53bd2bf9e6e1b8fcf320642897ce
Ajeet Singh <imajeet5@gmail.com>
2020-12-15 14:55:22 +0530

    change name to cooking app

---
+++ cooking-app/src/components/RecipeList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+export default function RecipeList() {
+    return (
+        <React.Fragment>
+            <Recipe/>
+            <Recipe/>
+        </React.Fragment>
+    )
+}
